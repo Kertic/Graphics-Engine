1614944ee58c63cf59ca2d410c3bf1240e90afe6
Kertic <coolbman200@gmail.com>
2017-11-02 14:25:23 -0400

    Fixed procedural pyramid bottom face not appearing

--- App2/App2/Content/Sample3DSceneRenderer.cpp
+++ App2/App2/Content/Sample3DSceneRenderer.cpp
@@ -687,8 +687,8 @@ void Sample3DSceneRenderer::CreateDeviceDependentResources()
 		static const unsigned short pyramidIndices[] =
 		{
 			//Bottom face
-			0,1,2,
-			2,3,0,
+			2,1,0,
+			0,3,2,
 
 			//+z
 			3,4,2,
